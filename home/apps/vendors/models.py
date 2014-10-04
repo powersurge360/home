0a0847160e157c05f2bbb071ffd210ce280c702c
Kevin Diale <kdiale@nerdery.com>
2014-10-04 00:31:10 -0400

    Added unicode methods to vendor models.

--- home/apps/vendors/models.py
+++ home/apps/vendors/models.py
@@ -5,6 +5,9 @@ class Service(models.Model):
     name = models.CharField(max_length=400)
     description = models.TextField(blank=True)
 
+    def __unicode__(self):
+        return self.name
+
 
 class Vendor(models.Model):
     name = models.CharField(max_length=400)
@@ -18,9 +21,15 @@ class Vendor(models.Model):
         blank=True,
     )
 
+    def __unicode__(self):
+        return self.name
+
 
 class Question(models.Model):
     question = models.TextField()
     answer = models.TextField(blank=True)
 
     vendor = models.ForeignKey(Vendor, related_name='questions')
+
+    def __unicode__(self):
+        return self.question
